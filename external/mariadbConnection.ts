0d5569e785a20fe935391e76c8812cef1dca7963
arianemadureira <arianemadureira@live.com>
2024-04-22 22:31:39 -0300

    add projeto

---
+++ external/mariadbConnection.ts
@@ -0,0 +1,81 @@
+import * as mariadb from 'mariadb';
+class MysqlConnection
+{
+
+    
+    connection: mariadb.Connection = null;
+
+    constructor(){
+        this.connect();
+    }
+
+    async connect(){
+        console.log( process.env.MARIADB_HOST);
+        this.connection = await mariadb.createConnection({            
+            host: process.env.MARIADB_HOST,
+            user: process.env.MARIADB_USER,
+            password: process.env.MARIADB_PASS,
+            database: process.env.MARIADB_DATABASE,
+            port : parseInt(process.env.MARIADB_PORT),
+        });
+
+        await this.connection.query(`CREATE TABLE IF NOT EXISTS cliente (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(200) not null,
+            email VARCHAR(245) not null unique,
+            cpf_cnpj VARCHAR(20) not null unique,
+            created datetime null,
+            modified datetime null
+        )  ENGINE=INNODB;`);
+
+        await this.connection.query(`CREATE TABLE IF NOT EXISTS categoria (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(200) not null unique,
+            created datetime null,
+            modified datetime null
+        )  ENGINE=INNODB;`);
+
+        await this.connection.query(`
+            CREATE TABLE IF NOT EXISTS produto (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                category_id INT not null,
+                title VARCHAR(200) not null unique,
+                description text null,
+                value decimal(19,2) not null default 0,
+                created datetime null,
+                modified datetime null,
+                FOREIGN KEY (category_id) REFERENCES categoria(id)
+            )  ENGINE=INNODB;
+        `);
+
+        await this.connection.query(`
+            CREATE TABLE IF NOT EXISTS pedidos (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                customer_id INT NOT NULL,
+                status VARCHAR(200) NOT NULL DEFAULT 'created',
+                total_value DECIMAL(19,2) NOT NULL DEFAULT 0,
+                created DATETIME NULL,
+                modified DATETIME NULL,
+                FOREIGN KEY (customer_id) REFERENCES cliente(id)
+            ) ENGINE=INNODB;
+        `);
+
+        await this.connection.query(`
+        CREATE TABLE IF NOT EXISTS pedido_produtos (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            order_id INT NOT NULL,
+            product_id INT NOT NULL,
+            created DATETIME NULL,
+            modified DATETIME NULL,
+            FOREIGN KEY (order_id) REFERENCES pedidos(id),
+            FOREIGN KEY (product_id) REFERENCES produto(id)
+        ) ENGINE=INNODB;
+        `);
+    }
+
+    public conn () {
+        return this.connection;
+    }
+}
+
+export default MysqlConnection
---
+++ server.ts
@@ -0,0 +1,46 @@
+import * as express from "express";
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import urls from './application/api/routes/urls';
+import Auth from "./application/api/middler/auth";
+import Logger from "./application/api/middler/logger";
+import userRouter from './application/api/routes/userRouter';
+import { IDataBase } from './interfaces/IDataBase';
+
+export default class Server{
+    public app: express.Application;
+
+    constructor (readonly dbconnection: IDataBase) {
+        this.app = express();
+        this.middler();
+        this.routes();
+    }
+
+    enableCors(){
+        const options: cors.CorsOptions = {
+            methods : "GET,OPTIONS,PUT,POST,DELETE",
+            origin: "*"
+        }
+        this.app.use(cors(options));
+    }
+
+    middler() {
+        this.enableCors();
+        this.app.use(bodyParser.json());
+        this.app.use(bodyParser.urlencoded({extended: false}));
+    }
+
+    routes() {
+        this.app.use(Logger.log);
+
+        this.app.route('/').get((req, res) => {
+            res.json({
+                'version' : '1.1.6',
+                'date' : '2024-03-18' 
+            });
+        });
+        this.app.use('/api/v1/', userRouter(this.dbconnection));
+        this.app.use(Auth.validate);
+        this.app.use("/", urls(this.dbconnection));
+    }
+}
