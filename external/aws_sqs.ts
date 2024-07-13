c183fb82d9afba482ced461aa7e045d063dbb2af
bruno-blauzius <147417627+bruno-blauzius@users.noreply.github.com>
2024-07-13 16:10:07 -0300

    Dev -> main (#17)
    * ajuste pipeline e inclusão sonarclound
    
    * teste pipeline
    
    * add test  repositories
    
    * remove arquivo errado
    
    * fix jest (#1)
    
    * Add testes
    
    * add testes
    
    * add tests
    
    * adicionado desenhos de novas estruturas
    
    * adicionado desenhos de novas estruturas (#3)
    
    Co-authored-by: Bruno Blauzius Schuindt <bruno.blauzius@lojacorr.local>
    
    * add more tests
    
    * Desenhos (#4)
    
    * adicionado desenhos de novas estruturas
    
    * adicionado desenhos de novas estruturas
    
    ---------
    
    Co-authored-by: Bruno Blauzius Schuindt <bruno.blauzius@lojacorr.local>
    
    * Add Bdd
    
    * Update main.yml
    
    * adicionado global 80 coverage e instalação babel-preset
    
    * videos
    
    * build dev/main e videos
    
    * version
    
    * processamento SQS com node express
    
    * remove bull consume queue
    
    * criado repository pedido entrega
    
    * adicionado fila de confirmação pedido e pedido cancelado
    
    * imagem do diagrama sqs e help de criação das filas
    
    * imagem do diagrama sqs e help de criação das filas
    
    * Fix pedido SQS
    
    * adicionado fila subscribe de pedido finalizado
    
    * Fix
    
    * adicionado a imagem e a justificativa do padrão Coreaografia
    
    * adicionado a imagem e a justificativa do padrão Coreaografia
    
    * Update README.md
    
    * Update README.md
    
    * ajuste diagrama
    
    * ajustes query de criação de banco de dados
    
    * pedidos controller
    
    * Update main.yml (#14)
    
    * Skip test
    
    * add tls
    
    * ajustado as credenciais em pedidos
    
    ---------
    
    Co-authored-by: Bruno Blauzius Schuindt <bruno.blauzius@LOJACORR.LOCAL>
    Co-authored-by: arianemadureira <arianemadureira@live.com>

--- external/aws_sqs.ts
+++ external/aws_sqs.ts
@@ -8,14 +8,18 @@ class AWSSQS
     queueUrl = process.env.AWS_SQS_URL;
 
     constructor() {
-        let configs = {region: "us-east-1",};
+        let configs = {
+            region: "us-east-1",
+            credentials: {
+                accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+            }
+        };
+        
         if (process.env.DEBUG=='true') {
             configs['endpoint'] = "http://localstack:4566";
-            configs['credentials'] =  {
-                accessKeyId: process.env.AWS_ACCESS_KEY_ID, // A chave de acesso não importa para o LocalStack
-                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, // A chave secreta não importa para o LocalStack
-            }
         } 
+        
         this.sqsClient = new SQSClient(configs);
     }
 
